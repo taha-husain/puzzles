aa5ee0da2292f977b3dfa1a6f78b455a184f5526
Taha Husain <tahahussain18@gmail.com>
2015-06-01 19:26:09 +0530

    Update sorting.rb

--- ignite/problem6/sorting.rb
+++ ignite/problem6/sorting.rb
@@ -5,3 +5,28 @@
 # and come up with the most efficient sorting solution you can think of.
 # Provide the space and time complexity of your algorithm
 
+require 'benchmark'
+
+arr = []
+1000000.times do
+  arr.push((rand*101).to_i)
+end
+
+# A pass through the MRI source code reveals that the the Array.sort! is implemented via quicksort. 
+# And while the benchmarks do show a four times performance hit when compared to the raw C++ version 
+# sorting an array of integers, you have to remember that an equivalent implementation in Ruby 
+# is operating on array of integer objects! 
+# In other words, native Array.sort! is a highly optimized piece of code: use it, embrace it, and don't worry about it!
+# via - https://www.igvita.com/2009/03/26/ruby-algorithms-sorting-trie-heaps/
+
+arr.sort!
+
+# Thus, time complexity is O(nlog n) on average and O(1) in best case.
+# Whereas, space complexity remains O(1) in best case and limits to O(log n) in average and worst case.
+
+# Benchmark.bm do |x|
+#  x.report do
+#    a.sort!
+#  end
+# end
+# 0.080000   0.000000   0.080000 (  0.079269)
